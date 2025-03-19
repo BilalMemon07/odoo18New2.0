43e3972bc98c9365e01f6c00b5707f040dca28f0
BilalMemon07 <bilalsaylani451@gmail.com>
2025-03-20 01:29:57 +0500

    add exchange app

---
+++ manual_currency_exchange_rate_18/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "Manual Currency Exchange Rate on ",
+    "version" : "17.0.0.0",
+    "depends" : ['base','account','purchase','sale_management','stock'],
+    "author": "Muhammad Bilal",
+    "summary": "",
+    "description": """
+    
+    """,
+  
+    'category': 'Accounting',
+    "website" : "",
+    "data" :[
+             "views/customer_invoice.xml",
+             "views/account_payment_view.xml",
+             "views/purchase_view.xml",
+             "views/sale_view.xml",
+        ],
+    'qweb':[],
+    "auto_install": False,
+    "installable": True,
+    'live_test_url':'',
+	"images":['static/description/Banner.gif'],
+    "license": "OPL-1",
+}
